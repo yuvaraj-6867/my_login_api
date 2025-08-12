a3097bb0c548eb515f29a8c5242d1c80b5b9321d
yuvaraj-6867 <yuvaraj6867@gmail.com>
2025-08-12 18:08:25 +0530

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to root_path, notice: "User created successfully."
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
     else
-      render :new
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
+    rescue => e
+      Rails.logger.error("Signup Error: #{e.message}")
+      render json: { error: "Internal server error" }, status: :internal_server_error
   end
 
   private
