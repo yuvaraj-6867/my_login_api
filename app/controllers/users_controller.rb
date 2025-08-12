ee2f91c750f18c9368e6e000a67f16a1044b4643
yuvaraj-6867 <yuvaraj6867@gmail.com>
2025-08-12 16:58:45 +0530

    v

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
-  # POST /signup
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(user_params)
-
     if @user.save
-      render json: { message: "User created successfully" }, status: :created
+      redirect_to root_path, notice: "User created successfully."
     else
-      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      render :new
     end
   end
 
