ce357b6fabb8906b7e6479592b6deff98ef3a28b
yuvaraj-6867 <yuvaraj6867@gmail.com>
2025-08-18 17:19:37 +0530

    message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,24 +1,23 @@
 class MessagesController < ApplicationController
-  skip_before_action :verify_authenticity_token # Disable CSRF for API requests (only for APIs)
+  skip_before_action :verify_authenticity_token
 
   def index
-    @messages = Message.all
-    render json: @messages
+    messages = Message.all
+    render json: messages
   end
 
   def create
-    @message = Message.new(message_params)
-
-    if @message.save
-      render json: @message, status: :created
+    message = Message.new(message_params)
+    if message.save
+      render json: message, status: :created
     else
-      render json: { errors: @message.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: message.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   private
 
   def message_params
-    params.require(:message).permit(:text, :from)
+    params.require(:message).permit(:content, :user_id) 
   end
 end
