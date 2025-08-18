1045ae1b5158c993f53474614c378c41ec2395cf
yuvaraj-6867 <yuvaraj6867@gmail.com>
2025-08-18 16:26:32 +0530

    e

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
   resources :messages, only: [:index, :create]
 
+  # Optional alias (but not needed since resources already give /messages)
   post "/message", to: "messages#create"
   get "/message", to: "messages#index"
 
+  # User signup (registration)
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
 
+  # User login/logout (sessions)
   get "/signin", to: "sessions#new"
   post "/signin", to: "sessions#create"
-
   delete "/logout", to: "sessions#destroy"
 
+  # Root
   root "welcome#index"
 end
